9cd1971c1fc7342e568f2042c05dac4fc529d352
mkoltsov <root@javabean.ru>
2015-05-23 20:12:53 +0200

    almost there

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-  /**
-   * Welcome to Pebble.js!
-   *
-   * This is where you write your app.
-   */
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
 
-  var UI = require('ui');
-  var Vector2 = require('vector2');
-  var ajax = require('ajax');
+var UI = require('ui');
+var Vector2 = require('vector2');
+var ajax = require('ajax');
 
 var Base64 = {
     // private property
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
     // public method for encoding
-    encode : function (input) {
+    encode: function (input) {
         var output = "";
         var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
         var i = 0;
@@ -37,7 +37,7 @@ var Base64 = {
         return output;
     },
     // public method for decoding
-    decode : function (input) {
+    decode: function (input) {
         var output = "";
         var chr1, chr2, chr3;
         var enc1, enc2, enc3, enc4;
@@ -63,15 +63,15 @@ var Base64 = {
         return output;
     },
     // private method for UTF-8 encoding
-    _utf8_encode : function (string) {
-        string = string.replace(/\r\n/g,"\n");
+    _utf8_encode: function (string) {
+        string = string.replace(/\r\n/g, "\n");
         var utftext = "";
         for (var n = 0; n < string.length; n++) {
             var c = string.charCodeAt(n);
             if (c < 128) {
                 utftext += String.fromCharCode(c);
             }
-            else if((c > 127) && (c < 2048)) {
+            else if ((c > 127) && (c < 2048)) {
                 utftext += String.fromCharCode((c >> 6) | 192);
                 utftext += String.fromCharCode((c & 63) | 128);
             }
@@ -84,27 +84,27 @@ var Base64 = {
         return utftext;
     },
     // private method for UTF-8 decoding
-    _utf8_decode : function (utftext) {
+    _utf8_decode: function (utftext) {
         var string = "";
         var i = 0;
         var c = 0;
 //        var c1 = 0;
         var c2 = 0;
         var c3 = 0;
-        while ( i < utftext.length ) {
+        while (i < utftext.length) {
             c = utftext.charCodeAt(i);
             if (c < 128) {
                 string += String.fromCharCode(c);
                 i++;
             }
-            else if((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i+1);
+            else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
                 i += 2;
             }
             else {
-                c2 = utftext.charCodeAt(i+1);
-                c3 = utftext.charCodeAt(i+2);
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
                 i += 3;
             }
@@ -113,100 +113,99 @@ var Base64 = {
     }
 };
 
-  var splashCard = new UI.Card({
+var splashCard = new UI.Card({
     title: 'Barracuda',
     subtitle: 'Let me serve you',
     body: 'Press any button.'
-  });
+});
 
-  splashCard.show();
+splashCard.show();
 
-  splashCard.on('click', 'up', function(e) {
-  var URL = 'https://api-eu.clusterpoint.com/741/barracuda/911/.json';  
+splashCard.on('click', 'up', function (e) {
+    var URL = 'https://api-eu.clusterpoint.com/741/barracuda/911/.json';
     // Download data
-    ajax({url: URL,
-          method: 'put',
-          type: 'json',
-          data: {"funcId":"alarm",
-                 "timestamp":new Date().getTime()
-                },
-          headers:{
-            Authorization: "Basic " + Base64.encode('root@javabean.ru' + ":" + '24801x'),
-          }
-    },
-      function(json) {
-//        function(json) {
-          var wind = new UI.Window({fullscreen: true});
-          var textfield = new UI.Text({
-            position: new Vector2(0, 65),
-            size: new Vector2(144, 30),
-            font: 'gothic-24-bold',
-            text: 'CALLING EMERGENCY!',
-            textAlign: 'center'
-          });
-          wind.add(textfield);
-          wind.show();
-        //splashCard.hide();
-      },
-       function(error) {
-         console.log('Ajax failed: ' + JSON.stringify(error));
-       }
+    ajax({
+            url: URL,
+            method: 'post',
+            type: 'json',
+            data: {
+                "query": "~911",
+                "docs": 2
+            },
+            headers: {
+                Authorization: "Basic " + Base64.encode('root@javabean.ru' + ":" + '24801x'),
+            }
+        },
+        function (json) {
+            var arr = []
+            for (key in json.documents) {
+                arr.push(key);
+            }
+
+            var menu = new UI.Menu({
+                sections: [{
+                    items: [
+                    {
+                        title: json.documents[arr[0]].id,
+                        subtitle: 'Can do Menus'
+                    },
+                    {
+                        title: json.documents[arr[1]].id,
+                        subtitle: 'Subtitle Text'
+                    }]
+            }]
+            });
+            menu.on('select', function (e) {
+                console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+                console.log('The item is titled "' + e.item.title + '"');
+            });
+            menu.show();
+            //splashCard.hide();
+        },
+        function (error) {
+            console.log('Ajax failed: ' + JSON.stringify(error));
+        }
     );
 
+});
 
-    // var menu = new UI.Menu({
-    //   sections: [{
-    //     items: [{
-    //       title: 'Pebble.js',
-    //       icon: 'images/menu_icon.png',
-    //       subtitle: 'Can do Menus'
-    //     }, {
-    //       title: 'Second Item',
-    //       subtitle: 'Subtitle Text'
-    //     }]
-    //   }]
-    // });
-    // menu.on('select', function(e) {
-    //   console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    //   console.log('The item is titled "' + e.item.title + '"');
-    // });
-    // menu.show();
-  });
-
-    splashCard.on('click', 'select', function(e) {
-    var URL = 'https://api-eu.clusterpoint.com/741/barracuda/911/.json';  
-      // Download data
-    ajax({url: URL,
-          method: 'put  ',
-          type: 'json',
-          data: {"funcId":"alarm",
-                 "timestamp":new Date().getTime()
-                },
-          headers:{
-            Authorization: "Basic " + Base64.encode('root@javabean.ru' + ":" + '24801x'),
-          }
-    },
-        function(json) {
-          var wind = new UI.Window({fullscreen: true});
-          var textfield = new UI.Text({
-            position: new Vector2(0, 65),
-            size: new Vector2(144, 30),
-            font: 'gothic-24-bold',
-            text: 'CALLING EMERGENCY!',
-            textAlign: 'center'
-          });
-          wind.add(textfield);
-          wind.show();
+splashCard.on('click', 'select', function (e) {
+    var URL = 'https://api-eu.clusterpoint.com/741/barracuda/911/.json';
+    // Download data
+    ajax({
+            url: URL,
+            method: 'put  ',
+            type: 'json',
+            data: {
+                "funcId": "alarm",
+                "timestamp": new Date().getTime()
+            },
+            headers: {
+                Authorization: "Basic " + Base64.encode('root@javabean.ru' + ":" + '24801x'),
+            }
         },
-        function(error) {
-         console.log('Ajax failed: ' + error);
-       }
-    );});
+        function (json) {
+            var wind = new UI.Window({fullscreen: true});
+            var textfield = new UI.Text({
+                position: new Vector2(0, 65),
+                size: new Vector2(144, 30),
+                font: 'gothic-24-bold',
+                text: 'CALLING EMERGENCY!',
+                textAlign: 'center'
+            });
+            wind.add(textfield);
+            wind.show();
+        },
+        function (error) {
+            console.log('Ajax failed: ' + error);
+        }
+    );
+});
 
-  splashCard.on('click', 'down', function(e) {
+splashCard.on('click', 'down', function (e) {
     var card = new UI.Card();
     card.title('About');
     card.subtitle('AngelHack rocks');
     card.body('We did this app through a course of this weekend');
     card.show();
-  });
+});
